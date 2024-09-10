4da4dcf29805c3ca5cb3213fe864ccb3207d0366
israeloriente <israelnunesoriente@gmail.com>
2024-09-10 01:05:10 +0100

    Aula 04 - Creating state to random numbers

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,8 +17,16 @@ class PaginaInicial extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-            title: Text('Gerador Aleatório'), backgroundColor: Colors.blue),
-        body: Center(child: Titulo()));
+            title: Text(
+              'Gerador Aleatório',
+              style: TextStyle(color: Colors.white),
+            ),
+            backgroundColor: Colors.blue),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [Titulo(), SizedBox(height: 30), NumeroAleatorio()],
+        )));
   }
 }
 
@@ -25,7 +34,34 @@ class Titulo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('Número Aleatório', style: TextStyle(fontSize: 20)),
-    );
+        child: Text('Gerador de Números', style: TextStyle(fontSize: 20)));
+  }
+}
+
+class NumeroAleatorio extends StatefulWidget {
+  @override
+  NumeroAleatorioState createState() => NumeroAleatorioState();
+}
+
+class NumeroAleatorioState extends State<NumeroAleatorio> {
+  int numero = 0;
+  void gerarNumero() {
+    print("Teste");
+    setState(() {
+      Random aleatorio = Random();
+      numero = aleatorio.nextInt(1000);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Column(
+      children: [
+        Text("$numero", style: TextStyle(fontSize: 30)),
+        SizedBox(height: 10),
+        ElevatedButton(onPressed: gerarNumero, child: Text("Gerar Número"))
+      ],
+    ));
   }
 }
